38410d7f9fe14220afde085300f67a42aba53290
Kwon-YJ <kyj1435@gmail.com>
2023-05-24 17:55:10 +0900

    start streamlit

---
+++ streamlit/day5.py
@@ -0,0 +1,40 @@
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+
+st.header("st.write")
+
+# ex 1
+
+st.write("Hello, *World!* :sunglasses:")
+
+# ex2
+
+st.write(1234)
+
+
+# ex3
+
+df = pd.DataFrame({
+    "first column": [1,2,3,4],
+    "second column": [10, 20, 30, 40]
+    })
+st.write(df)
+
+# ex 4
+
+st.write("Below is a DataFrame:", df, "Above is a dataframe.")
+
+# ex 5
+
+df2 = pd.DataFrame(
+    np.random.rand(200, 3),
+    columns=["a", "b", "c"])
+c = alt.Chart(df2).mark_circle().encode(
+    x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+st.write(c)
+
+
+np.random.rand()
